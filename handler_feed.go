50ec9506e804151c6b5987f63fb3533d41988879
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-05-28 01:19:49 +0800

    feat: add feed commands and RSS fetcher

---
+++ handler_feed.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/LuisCabantac/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerFollow(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <url>", cmd.Name)
+	}
+	url := cmd.Args[0]
+
+	ctx := context.Background()
+
+	currentUser := s.cfg.CurrentUserName
+	user, err := s.db.GetUser(ctx, currentUser)
+	if err != nil {
+		return fmt.Errorf("couldn't get user: %w", err)
+	}
+
+	feed, err := s.db.GetFeedByURL(ctx, url)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	feedFollows, err := s.db.GetFeedFollowsForUser(ctx, user.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	for _, ff := range feedFollows {
+		if ff.Url == feed.Url {
+			fmt.Printf("%s\n%s\n", ff.FeedName, ff.UserName)
+			return nil
+		}
+	}
+
+	feedFollow, err := s.db.CreateFeedFollow(ctx, database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed follow: %w", err)
+	}
+
+	fmt.Printf("%s\n%s\n", feedFollow.FeedName, feedFollow.UserName)
+
+	return nil
+}
+
+func handlerFollowing(s *state, cmd command) error {
+	ctx := context.Background()
+	currentUser := s.cfg.CurrentUserName
+
+	user, err := s.db.GetUser(ctx, currentUser)
+	if err != nil {
+		return fmt.Errorf("couldn't get user: %w", err)
+	}
+
+	feedFollows, err := s.db.GetFeedFollowsForUser(ctx, user.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed follows: %w", err)
+	}
+
+	for _, ff := range feedFollows {
+		fmt.Println(ff.FeedName)
+	}
+
+	return nil
+}
+
+func handlerFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feeds: %w", err)
+	}
+
+	for _, f := range feeds {
+		fmt.Printf("%s\n%s\n%s\n", f.Name, f.Url, f.UserName)
+	}
+
+	return nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.Args) != 2 {
+		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
+	}
+	name := cmd.Args[0]
+	url := cmd.Args[1]
+	ctx := context.Background()
+	currentUser := s.cfg.CurrentUserName
+
+	user, err := s.db.GetUser(ctx, currentUser)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	feed, err := s.db.CreateFeed(ctx, database.CreateFeedParams{
+		ID:        uuid.New(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't add feed: %w", err)
+	}
+
+	fmt.Println(feed, user)
+
+	_, err = s.db.CreateFeedFollow(ctx, database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't add feed follow: %w", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -43,6 +43,11 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handleUsers)
+	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", handlerAddFeed)
+	cmds.register("feeds", handlerFeeds)
+	cmds.register("follow", handlerFollow)
+	cmds.register("following", handlerFollowing)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
